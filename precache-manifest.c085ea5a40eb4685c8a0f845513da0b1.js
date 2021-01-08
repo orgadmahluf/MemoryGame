b008b6d93dc7ad65d683f09aa3a21e2b8a373a4a
orgad <orgad@svcollege.co.il>
2021-01-08 09:57:23 +0200

    Updates

---
+++ precache-manifest.c085ea5a40eb4685c8a0f845513da0b1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c768dc7478ccfbf0759dd2b0cd0c6fa0",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "25e5a9282eeb4aee9ecd",
+    "url": "/MemoryGame/static/css/2.ecf09bb4.chunk.css"
+  },
+  {
+    "revision": "85ef2285ce4ef50d960f",
+    "url": "/MemoryGame/static/css/main.fec24ef7.chunk.css"
+  },
+  {
+    "revision": "25e5a9282eeb4aee9ecd",
+    "url": "/MemoryGame/static/js/2.af5ba32b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MemoryGame/static/js/2.af5ba32b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ef2285ce4ef50d960f",
+    "url": "/MemoryGame/static/js/main.ea14e6df.chunk.js"
+  },
+  {
+    "revision": "5e0bb524f09eb35ed24b",
+    "url": "/MemoryGame/static/js/runtime-main.b3ed72af.js"
+  },
+  {
+    "revision": "2f59ceda5ca0cdcb160332c1ca28fd69",
+    "url": "/MemoryGame/static/media/bee.2f59ceda.jpg"
+  },
+  {
+    "revision": "f253c035fc3d33a8b5c3660d4294eeed",
+    "url": "/MemoryGame/static/media/bird.f253c035.jpg"
+  },
+  {
+    "revision": "a7a78448e23a0bb269baab7134d83a52",
+    "url": "/MemoryGame/static/media/gina.a7a78448.jpg"
+  },
+  {
+    "revision": "c6152bf1a370468db78c99122c657c17",
+    "url": "/MemoryGame/static/media/homeWallPaper.c6152bf1.jpg"
+  },
+  {
+    "revision": "c6152bf1a370468db78c99122c657c17",
+    "url": "/MemoryGame/static/media/homeWallPaper2.c6152bf1.jpg"
+  },
+  {
+    "revision": "78ac26104f7909efb06579d1f3c567a4",
+    "url": "/MemoryGame/static/media/horse.78ac2610.jpeg"
+  },
+  {
+    "revision": "c5438af5fffd4d19d9ebba9add998def",
+    "url": "/MemoryGame/static/media/kinor.c5438af5.jpg"
+  },
+  {
+    "revision": "fad24d0dd36b9ac33c552bd0a403e5d9",
+    "url": "/MemoryGame/static/media/parpar.fad24d0d.jpg"
+  },
+  {
+    "revision": "6822ef317aac20f0b83015d0a006661c",
+    "url": "/MemoryGame/static/media/updown.6822ef31.png"
+  }
+]);
